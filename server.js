b1cf297f3a76128e579acfbb76806f91bd2b8e51
Zoe Ferencova <zoeferencova@Zoes-MacBook-Pro-2.local>
2022-09-16 17:13:28 -0400

    tried hardcoding database url

--- server.js
+++ server.js
@@ -11,12 +11,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
+        connectionString: 'postgres://chbobuojnbgkvb:c81e37bdfbfaf8e8d80021e2bacd4edca1a57ef475564f3b8e1d0340dcc448d8@ec2-34-227-120-79.compute-1.amazonaws.com:5432/d5jtf8iag3dmk8',
         ssl: { rejectUnauthorized: false }
     }
 });
-
-console.log(process.env.DATABASE_URL)
 
 const app = express();
 
@@ -24,7 +22,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.json("success"));
+app.get('/', (req, res) => res.send(process.env.DATABASE_URL));
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/user/:id', user.handleGetUser(db));
