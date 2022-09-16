e7a4bc106a79a7c45af01a951375867591e2ca9e
Zoe Ferencova <zoeferencova@Zoes-MacBook-Pro-2.local>
2022-09-16 00:42:22 -0400

    tried new ssl option

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ const signin = require('./controllers/signin');
 const user = require('./controllers/user');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
+
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: process.env.DATABASE_URL ? true : false
     }
 });
 
-db.raw('select 1+1 as result').then(function () {
-    console.log('hi')
-  });
+// db.raw('select 1+1 as result').then(function () {
+//     console.log('hi')
+//   });
 
 const app = express();
 
