8a844e3bca3864bb9e5ab2c7134b153cb6704839
Zoe Ferencova <zoeferencova@Zoes-MacBook-Pro-2.local>
2022-09-16 16:10:20 -0400

    trying rejectUnauthorized alone

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    // connection: {
-    //     connectionString: process.env.DATABASE_URL,
-    //     ssl: false
-    // }
-    connection: process.env.DATABASE_URL
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized }
+    }
 });
 
 const app = express();
