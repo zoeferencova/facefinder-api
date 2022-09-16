41668af390e28ee31645a890ee8adbd91deee012
Zoe Ferencova <zoeferencova@Zoes-MacBook-Pro-2.local>
2022-09-16 01:04:21 -0400

    .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL + "?ssl=true?debug=true",
-        ssl: !!process.env.SSL
+        ssl: false
     }
 });
 
